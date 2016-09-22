16bdded92d83d29f36e7f5c62fbf7f2881b12519
sakib3 <sakib3@gmail.com>
2016-09-22 23:47:49 +0200

    make the heroku build green ?

--- _config.js
+++ _config.js
@@ -2,11 +2,13 @@ var config = {};
 
 config.mongoURI = {
   development: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost/veripankki',
+  production: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost/veripankki',
   test: process.env.TEST_DB_URL!==undefined ?process.env.TEST_DB_URL :'mongodb://localhost/veripankki-test'
 };
 
 config.serverPORT = {
   development: process.env.DEV_PORT!==undefined ?process.env.DEV_PORT :'3000',
+  production: process.env.DEV_PORT!==undefined ?process.env.DEV_PORT :'3000',
   test: process.env.TEST_PORT!==undefined ?process.env.TEST_PORT :'3000'
 };
 
