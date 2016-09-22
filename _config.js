426f7a8a71ef9f5c0af7cfb8be5711eb34c7283a
sakib3 <sakib3@gmail.com>
2016-09-23 00:20:22 +0200

    added dynamic port config for heroku

--- _config.js
+++ _config.js
@@ -1,15 +1,15 @@
 var config = {};
 
 config.mongoURI = {
-  development: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost/veripankki',
-  production: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost/veripankki',
-  test: process.env.TEST_DB_URL!==undefined ?process.env.TEST_DB_URL :'mongodb://localhost/veripankki-test'
+  development: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost:27017/veripankki',
+  production: process.env.DEV_DB_URL!==undefined ?process.env.DEV_DB_URL :'mongodb://localhost:27017/veripankki',
+  test: process.env.TEST_DB_URL!==undefined ?process.env.TEST_DB_URL :'mongodb://localhost:27017/veripankki-test'
 };
 
 config.serverPORT = {
-  development: process.env.DEV_PORT!==undefined ?process.env.DEV_PORT :'3000',
-  production: process.env.DEV_PORT!==undefined ?process.env.DEV_PORT :'3000',
-  test: process.env.TEST_PORT!==undefined ?process.env.TEST_PORT :'3000'
+  development: process.env.DEV_PORT!==undefined ?process.env.PORT :'8080',
+  production: process.env.DEV_PORT!==undefined ?process.env.PORT :'8080',
+  test: process.env.TEST_PORT!==undefined ?process.env.PORT :'8080'
 };
 
 config.dropDBOnStartUp = {
