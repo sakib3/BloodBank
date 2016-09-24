394d0427efdc8a866a6eb34a90b74d41068ad357
sakib3 <sakib3@gmail.com>
2016-09-24 10:00:22 +0200

    grunt watch on only one file to solve timout ?

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,8 @@ module.exports = function(grunt) {
     },
     watch: {
         express: {
-          files:  [ '**/*.js' ],
+          //files:  [ '**/*.js' ],
+          files:  [ 'server.js' ],
           tasks:  [ 'express:development' ],
           options: {
             spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
