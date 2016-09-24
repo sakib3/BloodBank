85a574132b5416bee1c2dcbf306be1121db6fd38
sakib3 <sakib3@gmail.com>
2016-09-24 21:21:22 +0200

    make the script right for local unit testing

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@
 mongod_process_id=$(pgrep mongod)
 if [[ -z $mongod_process_id ]]; then
     #mongod
+    echo 'mongod'
 fi
 
-eval cat ../bloodBankExportENV.sh
+eval `cat ../bloodBankExportENV.sh`
 # running test
 npm test
 #running server
